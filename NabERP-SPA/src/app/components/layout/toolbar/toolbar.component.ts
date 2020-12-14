e177df1e795ab2ba0b3c1efef4dd9e4dbe54ed09
kjalalifr <koorosh.jalali@outlook.com>
2020-12-14 15:13:20 +0330

    Initial Commit

---
+++ NabERP-SPA/src/app/components/layout/toolbar/toolbar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * @title Basic toolbar
+ */
+
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss']
+})
+export class ToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+
+
